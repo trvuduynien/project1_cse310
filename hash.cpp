741c98d29fd8b014f72a828102c419be546b89f2
trvuduynien <vtruon5@asu.edu>
2024-02-21 19:27:06 -0700

    change hash.h

---
+++ hash.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include "hash.h"
+#include "linked_list.hpp"
+
+HashTable::HashTable(int size)  {
+    this->size = size;
+}
+
+
+
+
+
+
--- hash.h
+++ hash.h
@@ -1,8 +1,23 @@
+#ifndef HASH_H
+#define HASH_H
 #include <iostream>
-#include <string>
-#include <cmath>
 // You are free to use additional libraries as long as it's not PROHIBITED per instruction.
 
-using namespace std;
 
 int hash_function(string text);
+
+class Hash_Table {
+    private:
+    int size;
+    //define an array of linked lists
+    LinkedList<string>* linked_lists;
+
+
+    public:
+    Hash_Table(int size);
+    int Hash_function(string text);
+    void Hash_insert(string text);
+    Linkedlist<string> Get_key(int index);
+
+}
+#endif
