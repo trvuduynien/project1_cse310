0eb0428e1d3e2d1ddfb4f419db52689fc632d075
Chickeninvader <96258758+Chickeninvader@users.noreply.github.com>
2024-02-21 19:08:10 -0700

    add linked_list template

---
+++ linked_list.hpp
@@ -0,0 +1,184 @@
+#ifndef LINKED_LIST_HPP
+#define LINKED_LIST_HPP
+
+#include <iostream>
+using namespace std;
+
+// Node template
+template <typename T>
+class Node
+{
+private:
+  T data;
+  Node<T> *next = nullptr;
+
+public:
+  // Constructor
+  Node();
+  Node(T data);
+  ~Node();
+
+  // Getters and Setters
+  T getData();
+  void setData(T dataNew);
+  Node<T> *getNext();
+  void setNext(Node<T> *dataNext);
+};
+
+// Constructor
+template <typename T>
+Node<T>::Node() : data(T()), next(nullptr) {}
+
+template <typename T>
+Node<T>::Node(T data) : data(data), next(nullptr) {}
+
+// Destructor
+template <typename T>
+Node<T>::~Node()
+{
+  next = nullptr;
+  data = T();
+}
+
+// Get and set method
+template <typename T>
+T Node<T>::getData()
+{
+  return data;
+}
+
+template <typename T>
+void Node<T>::setData(T dataNew)
+{
+  data = dataNew;
+}
+
+template <typename T>
+Node<T> *Node<T>::getNext()
+{
+  return next;
+}
+
+template <typename T>
+void Node<T>::setNext(Node<T> *dataNext)
+{
+  next = dataNext;
+}
+
+// LinkedList template
+template <typename T>
+class LinkedList
+{
+private:
+  Node<T> *head;
+  int size;
+
+public:
+  // Constructor
+  LinkedList();
+  // Destructor
+  ~LinkedList();
+
+  // Add methods
+  void AddNode(T data);
+
+  // Remove methods
+  T RemoveNode();
+
+  // Other methods
+  bool ElementExists(T data);
+  T RemoveAtIndex(int index);
+  Node<T> *Find(T data);
+  int Length();
+};
+
+// Constructor
+template <typename T>
+LinkedList<T>::LinkedList() : head(nullptr), size(0) {}
+
+// Destructor
+template <typename T>
+LinkedList<T>::~LinkedList()
+{
+  while (head != nullptr)
+  {
+    Node<T> *temp = head->getNext();
+    delete head;
+    head = temp;
+  }
+}
+
+// Add to the head, tail, or at a random index
+template <typename T>
+void LinkedList<T>::AddNode(T data)
+{
+  if (this->head == nullptr)
+  {
+    this->head = new Node<T>(data);
+  }
+  else
+  {
+    Node<T> *temp = this->head;
+    for (int i = 0; i < this->size - 1; i++)
+    {
+      temp = temp->getNext();
+    }
+    Node<T> *node_to_add = new Node<T>(data);
+    temp->setNext(node_to_add);
+    node_to_add->setPrevious(temp);
+  }
+  this->size++;
+}
+
+template <typename T>
+T LinkedList<T>::RemoveNode()
+{
+  if (this->head == nullptr)
+  {
+    throw std::out_of_range("Attempted to remove from an empty list");
+  }
+
+  if (this->size == 1)
+  {
+    T temp_data = this->head->getData();
+    delete this->head;
+    this->head = nullptr;
+    this->size--;
+    return temp_data;
+  }
+
+  Node<T> *temp = this->head;
+  for (int i = 0; i < this->size - 2; i++)
+  {
+    temp = temp->getNext();
+  }
+  T temp_data = temp->getNext()->getData();
+  delete temp->getNext();
+  temp->setNext(nullptr);
+
+  this->size--;
+  return temp_data;
+}
+
+template <typename T>
+Node<T> *LinkedList<T>::Find(T data)
+{
+  Node<T> *temp = this->head;
+  for (int i = 0; i < this->size; i++)
+  {
+    if (temp->getData() == data)
+    {
+      return temp;
+    }
+    temp = temp->getNext();
+  }
+  return nullptr;
+}
+
+template <typename T>
+int LinkedList<T>::Length()
+{
+  return this->size;
+}
+
+#endif
