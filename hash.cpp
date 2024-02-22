f41f662612f123438d5ae4800b10b61276f09693
trvuduynien <vtruon5@asu.edu>
2024-02-21 20:24:26 -0700

    hash_function update

--- hash.cpp
+++ hash.cpp
@@ -4,12 +4,12 @@
 #include "hash.h"
 #include "linked_list.hpp"
 
-HashTable::HashTable(int size)  {
-    this->size = size;
+using namespace std;
+
+int Hash_Table::Hash_function(string text){
+    
+    this->size = size; 
+    // number of characters in the input string
+    int num_chars = text.length(); 
+    return num_chars % size; 
 }
-
-
-
-
-
-
