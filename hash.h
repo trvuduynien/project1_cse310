d5152ad092a2db2893d4db3f886e44854ef263f0
trvuduynien <vtruon5@asu.edu>
2024-02-22 19:54:02 -0700

    add std function

--- hash.h
+++ hash.h
@@ -9,12 +9,15 @@ private:
     int size;
     // define an array of linked lists
     LinkedList<string> *linked_lists;
+    int num_elements;
 
 public:
     Hash_Table(int size);
     int Hash_function(string text);
     void Hash_insert(string text);
     LinkedList<string> *Get_slot(int index);
+    //define function calculate Standard deviation
+    float Standard_deviation();
     void Print_content(int length);
 }
 
