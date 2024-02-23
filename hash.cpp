5685e5f60938fde30d5335c218d9a26af0944eea
Chickeninvader <96258758+Chickeninvader@users.noreply.github.com>
2024-02-22 19:52:21 -0700

    add print function

--- hash.cpp
+++ hash.cpp
@@ -18,13 +18,27 @@ void Hash_Table::Hash_insert(string text)
     int index = this->Hash_function(text);
     this->linked_lists[index].AddNode(text);
 }
-LinkedList<string> *Hash_Table::Get_slot(int index)
+LinkedList<string> Hash_Table::Get_slot(int index)
 {
     return this->linked_lists[index];
 }
 void Hash_Table::Print_content(int length)
 {
     for (int index = 0; index < length; index++)
+    {
+        LinkedList<string> list_str = this->Get_slot(index);
+        printf("Slot %d:", index);
+        for (int node_index = 0; node_index < list_str.Length(); node_index++)
+        {
+            string temp = list_str.Retrieve(node_index);
+            printf("%s ", temp);
+        }
+        cout << endl;
+    }
+}
+void Hash_Table::Print_slot_length(int index)
+{
+    for (int index = 0; index < this->size; index++)
     {
         LinkedList<string> list_str = this->Get_slot(index);
         printf("Slot %d:", index);
