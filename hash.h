5072264da378919c06435e0fa97427a1e2713408
Chickeninvader <96258758+Chickeninvader@users.noreply.github.com>
2024-02-22 19:52:01 -0700

    add print function and fix error

--- hash.h
+++ hash.h
@@ -14,8 +14,9 @@ public:
     Hash_Table(int size);
     int Hash_function(string text);
     void Hash_insert(string text);
-    LinkedList<string> *Get_slot(int index);
+    LinkedList<string> Get_slot(int index);
     void Print_content(int length);
-}
+    void Print_slot_length(int length);
+};
 
 #endif
